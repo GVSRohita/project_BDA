a29554d84c6535c894e10df23729801a40d58c83
charantej224 <charantej.career@gmail.com>
2021-02-16 18:54:07 -0500

    multi-level classification

--- apps/sentiment_analysis_main.py
+++ apps/sentiment_analysis_main.py
@@ -4,26 +4,44 @@ import os
 import pandas as pd
 import numpy as np
 
-root_dir = "/home/charan/Documents/workspaces/python_workspaces/Data/BDA_Project"
-final_data = os.path.join(root_dir, "Sentiment_Financial_Data/sentiment_data_processed.csv")
-mapping_dict = os.path.join(root_dir, "Sentiment_Financial_Data/label_mapping_dict.json")
-load_model_path = os.path.join(root_dir, 'sentiment_state_dict_9.pt')
-news_classify_path = os.path.join(root_dir, "news_data/news_classification.csv")
+from utils.file_utils import read_json
+
+root_dir = "/home/charan/DATA/311_Data/multi-level-classification"
+final_data = os.path.join(root_dir, "balanced_multi-level.csv")
+updated_data = os.path.join(root_dir, "balanced_multi-level_update.csv")
+cat_json = os.path.join(root_dir, "category_class.json")
+type_json = os.path.join(root_dir, "type_class.json")
+cat_json = read_json(cat_json)
+type_json = read_json(type_json)
+load_model_path = ""
 
 
-def train_financial_news_sentiments():
+def setup_data(input_df):
+    input_df["label"] = input_df["TYPE"].apply(lambda x: type_json[x])
+    input_df["u_id"] = input_df.index
+    input_df.rename(columns={"Description": "desc"}, inplace=True)
+    input_df.to_csv(updated_data, index=False)
+    return input_df
+
+
+def train_classification():
     classification_df = pd.read_csv(final_data)
+    classification_df = setup_data(classification_df)
     number_of_classes = len(list(classification_df['label'].unique()))
-    model_directory = os.path.join(root_dir, "sentiment_state_dict")
+    model_directory = os.path.join(root_dir, "classify_state_dict")
     metrics_json = os.path.join(root_dir, "accuracy_metrics.json")
     training_loader, testing_loader = load_datasets(classification_df, train_size=0.8,
                                                     number_of_classes=number_of_classes)
-    start_epochs(training_loader, testing_loader, metrics_json, model_directory, epochs=10,
-                 number_of_classes=number_of_classes)
+    unique_ids, val_targets, val_outputs = start_epochs(training_loader, testing_loader, metrics_json, model_directory,
+                                                        epochs=50, number_of_classes=number_of_classes)
+    out_numpy = np.concatenate((unique_ids.reshape(-1, 1), val_targets.reshape(-1, 1), val_outputs.reshape(-1, 1)),
+                               axis=1)
+    predicted_df = pd.DataFrame(out_numpy, columns=['id', 'original', 'predicted'])
+    predicted_df.to_csv(os.path.join(root_dir, "predicted.csv"), index=False, header=True)
 
 
 def inference_classification():
-    inference_df = pd.read_csv(news_classify_path)
+    inference_df = pd.read_csv(final_data)
     test_loader = load_test_datasets(inference_df, 3)
     unique_ids, predictions = load_model(load_model_path, test_loader, 3)
     out_numpy = np.concatenate((unique_ids.reshape(-1, 1), predictions.reshape(-1, 1)), axis=1)
@@ -32,7 +50,4 @@ def inference_classification():
 
 
 if __name__ == '__main__':
-    inference_classification()
-
-# load_model_path = os.path.join(root_dir, 'prob_state_dict2.pt')
-# load_model(load_model_path, training_loader, testing_loader)
+    train_classification()
